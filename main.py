f37063669aa7673cc5991e309f7e78bb8df09647
Ndre <sonkych@Ndres-MBP.lan>
2023-05-05 10:12:08 +0300

    added User model

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = FastAPI(title="HC App")
+
+users = [
+    {"id": 1, "name": "Andrei"},
+    {"id": 2, "name": "Sofja"},
+    {"id": 3, "name": "Egor"},
+    {"id": 4, "name": "Makar"}
+]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    email: str
+    phone: str
+    telegram: str
+    access: str
+    department: str
+    position: str
+    password: str
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/users")
+def get_users():
+    return users
+
+
+@app.get("/users/{users_id}")
+def get_user(user_id: int):
+    try:
+        return [user for user in users if user.get("id") == user_id]
+    except IndexError:
+        return f"no user with id: {user_id}"
+
+
+@app.post("/users/{users_id}")
+def change_user_data(user_id: int, new_name: str):
+    current_user = next(user for user in users if user.get("id") == user_id)
+    current_user["name"] = new_name
+    return {"status": 200, "data": current_user}
+
